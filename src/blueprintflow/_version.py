4a518dcc48c06fa755911eba6d249df81bd63819
oceord <73301852+oceord@users.noreply.github.com>
2025-03-06 21:46:20 +0000

    Create _version file

---
+++ src/blueprintflow/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides blueprintflow version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update blueprintflow` to change this file.
+
+from incremental import Version
+
+__version__ = Version("blueprintflow", 25, 3, 0, dev=0)
+__all__ = ["__version__"]
