164cd86d5108061309cccdc63cd6bd5384aa6cef
oceord <73301852+oceord@users.noreply.github.com>
2025-08-20 14:45:47 +0000

    Add user data dir reset method

--- src/blueprintflow/utils/xdg/data.py
+++ src/blueprintflow/utils/xdg/data.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 from blueprintflow.utils.xdg.constants import (
@@ -45,31 +46,38 @@ class UserData:
     the necessary directories and files are created and accessible.
 
     Attributes:
-        _user_data_dir (Path): The path to the user data directory.
+        user_data_dir (Path): The path to the user data directory.
         log_file (Path): The path to the log file within the user data directory.
         lancedb_path (Path): The path to the LanceDB file within the user data
             directory.
     """
 
-    def __init__(self, user_data_dir: Path | None = None) -> None:
+    def __init__(
+        self, user_data_dir: Path | None = None, *, reset_if_exists: bool = False
+    ) -> None:
         """Initialize the UserData instance.
 
         This constructor initializes the user data directory and ensures that
         the necessary files and subdirectories are created. If a user data directory
         is not provided, it defaults to the platform-specific user data directory.
+        The directory may be reinitialized if reset_if_exists is True.
 
         Args:
             user_data_dir (Path, optional): The path to the user data directory.
                 If not provided, the default platform-specific user data directory is
                 used. Defaults to None.
+            reset_if_exists (bool, optional): If True, reinitialize the user data
+                directory. Defaults to False.
         """
-        self._user_data_dir = (
+        self.user_data_dir = (
             user_data_dir or get_platform_user_data_dir() / BPF_USER_DATA_DIR_NAME
         )
-        if not self._user_data_dir.exists():
+        self.log_file = self.user_data_dir / BPF_LOG_FILE_NAME
+        self.lancedb_path = self.user_data_dir / BPF_LANCEDB_DIR_NAME
+        if not self.user_data_dir.exists():
             self._init_user_data_dir()
-        self.log_file = self._user_data_dir / BPF_LOG_FILE_NAME
-        self.lancedb_path = self._user_data_dir / BPF_LANCEDB_DIR_NAME
+        elif reset_if_exists:
+            self.reset()
 
     def _init_user_data_dir(self) -> None:
         """Initialize the user data directory.
@@ -80,4 +88,19 @@ class UserData:
         Args:
             data_dir (Path): The path to the user data directory.
         """
-        self._user_data_dir.mkdir(parents=True, exist_ok=True)
+        self.user_data_dir.mkdir(parents=True, exist_ok=True)
+
+    def reset(self) -> None:
+        """Reset the user data directory to its initial state.
+
+        This method removes the existing user data directory and all its contents,
+        then reinitializes it as an empty directory. This is useful for cleaning up
+        or resetting the state of the user data directory, such as during testing
+        or when starting fresh.
+
+        Note:
+            This operation is irreversible and will delete all files and
+            subdirectories within the user data directory.
+        """
+        shutil.rmtree(self.user_data_dir, ignore_errors=True)
+        self._init_user_data_dir()
